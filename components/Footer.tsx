19ce81f8f31b5dd13fbb35bd9c1409ea8d747134
kaytee07 <kofi.taylor07@gmail.com>
2025-01-27 08:23:30 +0000

    gallery page created

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import { navigation } from '@/data'
 import React from 'react'
 
 const Footer = () => {
@@ -5,21 +6,15 @@ const Footer = () => {
     <section className="bg-black absolute w-full">
     <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
-            <div className="px-5 py-2">
-                <a href="http://instagram.com/worldg3nius" className="text-base leading-6 text-white hover:text-white-900">
-                    Press
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-white hover:text-white-900">
-                    Gallery
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-white hover:text-white-900">
-                    Shop
-                </a>
-            </div>
+            {
+                navigation.map((item, index) => (
+                    <div key={index} className="px-5 py-2">
+                        <a href={item.url} className="text-base leading-6 text-white hover:text-white-900">
+                            {item.name}
+                        </a>
+                    </div>
+                ))
+            }
             
         </nav>
         <div className="flex justify-center mt-8 space-x-6">
