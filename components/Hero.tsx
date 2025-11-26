6821010f3a82021068f2ddcb03aa84df8c5c821f
kaytee07 <kofi.taylor07@gmail.com>
2025-11-26 16:18:25 +0000

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="">
-      <div className="sideInfo flex justify-center relative max-sm:bg-[url(/img/discpv.png)] md:bg-[url(/img/disccv.png)] bg-cover bg-center h-[36rem] w-[100%]">
+      <div className="sideInfo flex justify-center relative max-sm:bg-[url(/img/websiteheadermobilepopup.png)] md:bg-[url(/img/websiteheaderpopup.png)] bg-cover bg-center h-[36rem] w-[100%]">
           {/* <button className="absolute bg-white text-black w-36 border-[1px] bottom-7 border-white">Shop Now</button> */}
         </div>
     </div>
--- components/SecondHero.tsx
+++ components/SecondHero.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 const SecondHero = () => {
   return (
     <div className="">
-      <div className="sideInfo flex justify-center relative max-sm:bg-[url(/img/fatgpv.png)] md:bg-[url('/img/fatgcv.png')] bg-cover bg-center h-[32rem] w-[100%]">
+      <div className="sideInfo flex justify-center relative 
+  max-sm:bg-[url('/img/websiteheaderaccratourmobile2.png')] 
+  md:bg-[url('/img/websiteheader2accratour.png')] 
+  bg-contain bg-no-repeat bg-center 
+  h-[32rem] w-[100%]">
             {/* <button className="absolute bg-white text-black w-36 border-[1px] bottom-7 border-white">Shop Now</button> */}
         </div>
     </div>
   )
 }
-
 export default SecondHero
